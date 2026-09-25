e4dc158454fab9c9e243533975de728c44d2f356
enjoyplayer666-beep <enjoyplayer666@gmail.com>
2026-09-25 21:30:49 +0300

    Create ClanSettingsHolder.java

---
+++ src/main/java/ru/yourname/clans/gui/ClanSettingsHolder.java
@@ -0,0 +1,13 @@
+package ru.yourname.clans.gui;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class ClanSettingsHolder implements InventoryHolder {
+    private Inventory inventory;
+    private final String clanName;
+    public ClanSettingsHolder(String clanName) { this.clanName = clanName; }
+    public String getClanName() { return clanName; }
+    public void setInventory(Inventory inventory) { this.inventory = inventory; }
+    @Override public Inventory getInventory() { return inventory; }
+}
